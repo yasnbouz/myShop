c04b66b1933cb5008a001be6a5b9c0afecd70a8b
Yassine El Bouziady <elbouziady.yassine@gmail.com>
2021-09-27 16:02:16 +0100

    chore: shopify setup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const WindiCSS = require('windicss-webpack-plugin').default;
 
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['cdn.shopify.com'],
+  },
   webpack(config) {
     config.plugins.push(new WindiCSS());
     return config;

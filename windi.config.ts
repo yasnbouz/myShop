1612b52510cde08573f071c271d5369d1901d0a7
Yassine El Bouziady <elbouziady.yassine@gmail.com>
2022-10-03 11:18:16 +0100

    feat(hero): blur style

--- windi.config.ts
+++ windi.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from 'windicss/helpers';
 
 export default defineConfig({
   darkMode: `class`,
-  theme: {},
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        hero: `linear-gradient(225deg, #21D4FD 0%, #B721FF 100%)`,
+      }),
+    },
+  },
   extract: {
     include: [`src/**/*.{ts,js,tsx,css}`],
     exclude: [`node_modules`, `.git`, `.next`],
   },
+  plugins: [],
 });

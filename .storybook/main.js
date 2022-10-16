bbdaf69cd8954cb99674bfc3299fa7c019a95bcd
Yassine El Bouziady <45861586+yasnbouz@users.noreply.github.com>
2022-10-16 17:36:18 +0100

    feat: add chromatic to deploy storybook (#9)
    * feat: deploy storybook with chromatic

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,13 +3,18 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const WindiCSS = require('windicss-webpack-plugin');
 
 module.exports = {
-  staticDirs:['../public'],
+  staticDirs: ['../public'],
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-a11y'],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
   },
+  env: (config) => ({
+    ...config,
+    NEXT_PUBLIC_SHOPIFY_STORE_ID: process.env.NEXT_PUBLIC_SHOPIFY_STORE_ID,
+    NEXT_PUBLIC_SHOPIFY_API_TOKEN: process.env.NEXT_PUBLIC_SHOPIFY_API_TOKEN,
+  }),
   webpackFinal: async (config) => {
     config.plugins.push(
       new WindiCSS({

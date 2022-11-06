91126f0cff22f3c84836651d7da48e2952f3f27b
Yassine El Bouziady <elbouziady.yassine@gmail.com>
2022-11-06 20:52:38 +0100

    perf: delete react-icons
    not supporting tree-shaking

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,7 +1,5 @@
 import { Dispatch, Fragment, SetStateAction } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
-import { MdClose } from 'react-icons/md';
-import { IoTrashOutline } from 'react-icons/io5';
 import Link from 'next/link';
 import useShopify from '@/hooks/useShopify';
 import Image from 'next/future/image';
@@ -49,7 +47,9 @@ export default function Cart({ cartOpen, setCartOpen }: CartProps) {
                     <div className="ml-3 h-7 flex items-center">
                       <button type="button" className="-m-2 p-2 text-gray-400 hover:text-gray-500" onClick={() => setCartOpen(false)}>
                         <span className="sr-only">Close panel</span>
-                        <MdClose className="h-6 w-6 align-middle" aria-hidden="true" />
+                        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
                       </button>
                     </div>
                   </div>
@@ -99,7 +99,20 @@ export default function Cart({ cartOpen, setCartOpen }: CartProps) {
 
                                   <div className="flex">
                                     <button name="trash" onClick={() => removeItem(product.id)} type="button" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                      <IoTrashOutline size={24} className="text-red-600" />
+                                      <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth="1.5"
+                                        stroke="currentColor"
+                                        className="w-6 h-6 text-red-600"
+                                      >
+                                        <path
+                                          strokeLinecap="round"
+                                          strokeLinejoin="round"
+                                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                                        />
+                                      </svg>
                                     </button>
                                   </div>
                                 </div>
